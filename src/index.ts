694053b049c6df3a1c5f3e769d1a2d9320f99500
Arimil <renari@arimil.com>
2020-09-21 07:01:09 -0700

    refactor(twitchnotifications): move twitch notifications out of index

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
-import axios from 'axios';
 import Discord from 'discord.js';
 import logger from './logger';
 import nedb from 'nedb';
 import path from 'path';
-import { TwitchAPI } from './twitchapi';
+import youtubeNotification from './notification-handlers/youtube-notification';
 
 // message handlers
 import anidbHandler from './message-handlers/anidb-handler';
@@ -14,10 +13,6 @@ import pixivHandler from './message-handlers/pixiv-handler';
 export default class Shubot {
   public static readonly version: string = '<version>';
   private readonly discordClient: Discord.Client;
-  private readonly discordGuildId: string = '717558249435562035';
-  private readonly discordClipChannelId: string = '717558882976661636';
-  private readonly twitchClipRefreshRate = 30000;
-  private lastClipDate: Date = new Date(0);
   private readonly messageHandlers: messageHandler[] = [];
   private readonly database: nedb;
 
@@ -38,19 +33,6 @@ export default class Shubot {
 
     // connect to discord
     this.discordClient.login(process.env.DISCORD_TOKEN).catch(Shubot.log.error);
-    this.getTwitchClips()
-      .then(clips => {
-        if (clips.length > 0) {
-          this.lastClipDate = new Date(clips[0].created_at);
-        } else {
-          // there are no clips, set the last clip date to now
-          this.lastClipDate = new Date();
-        }
-      })
-      .catch(err => {
-        Shubot.log.error(err);
-        this.shutdown(1);
-      });
   }
 
   private ready(): void {
@@ -63,7 +45,10 @@ export default class Shubot {
 
     this.discordClient.on('message', this.messageHandler.bind(this));
 
-    setInterval(this.checkTwitchClips.bind(this), this.twitchClipRefreshRate);
+    // post new twitch clips in discord
+    if (process.env.TWITCH_CLIENT_ID) {
+      new twitchNotification(this.discordClient, process.env.TWITCH_CLIENT_ID);
+    }
   }
 
   private messageHandler(message: Discord.Message): void {
@@ -73,60 +58,6 @@ export default class Shubot {
     this.messageHandlers.forEach(handler => {
       handler.handle(message);
     });
-  }
-
-  private getTwitchClips(): Promise<TwitchAPI.Clip[]> {
-    return axios
-      .get('https://api.twitch.tv/kraken/clips/top?channel=Leaflit&period=day&limit=100', {
-        headers: {
-          Accept: 'application/vnd.twitchtv.v5+json',
-          'Client-ID': process.env.TWITCH_CLIENT_ID,
-        },
-      })
-      .then(res => {
-        // sort the clips by date
-        const clips: TwitchAPI.Clip[] = res.data.clips || [];
-        clips.sort((first, second) => {
-          const firstDate = new Date(first.created_at).getTime();
-          const secondDate = new Date(second.created_at).getTime();
-          return -(firstDate - secondDate);
-        });
-        return clips;
-      });
-  }
-
-  private checkTwitchClips(): void {
-    this.getTwitchClips()
-      .then(clips => {
-        const clipstopost: TwitchAPI.Clip[] = [];
-        // check for new clips
-        for (let i = 0; i < clips.length; i++) {
-          // since clips is sorted by date ignore older clips
-          if (new Date(clips[i].created_at).getTime() <= this.lastClipDate.getTime()) {
-            break;
-          } else {
-            Shubot.log.info(`Found '${clips[i].title}'`);
-            clipstopost.push(clips[i]);
-          }
-        }
-        // post the new clips in discord
-        const guild = this.discordClient.guilds.cache.get(this.discordGuildId);
-        const channel = guild?.channels.cache.get(this.discordClipChannelId);
-        if (channel?.type === 'text') {
-          clipstopost.forEach(clip => {
-            (channel as Discord.TextChannel)
-              .send(`https://clips.twitch.tv/${clip.slug}`)
-              .catch(Shubot.log.error);
-          });
-        } else {
-          Shubot.log.error('Unable to find clips channel');
-        }
-        // update the latest clip to the newest one
-        if (clips.length > 0) {
-          this.lastClipDate = new Date(clips[0].created_at);
-        }
-      })
-      .catch(Shubot.log.error);
   }
 
   private shutdown(code = 0): void {
---
+++ src/notification-handlers/twitch-notification.ts
@@ -0,0 +1,78 @@
+import axios from 'axios';
+import Discord from 'discord.js';
+import notificationHandler from './notification-handler';
+import { TwitchAPI } from '../interfaces/twitchapi';
+import Shubot from '..';
+
+export default class twitchNotification extends notificationHandler {
+  private readonly discordClipChannelId: string = '717558882976661636';
+  private readonly twitchClientId: string;
+  private readonly twitchClipRefreshRate = 30000;
+  private lastClipDate: Date = new Date(0);
+
+  constructor(discordClient: Discord.Client, clientId: string) {
+    super(discordClient);
+    this.twitchClientId = clientId;
+
+    // TODO: change this to store the clip date in the database
+    this.getTwitchClips()
+      .then(clips => {
+        if (clips.length > 0) {
+          this.lastClipDate = new Date(clips[0].created_at);
+        } else {
+          // there are no clips, set the last clip date to now
+          this.lastClipDate = new Date();
+        }
+        setInterval(this.checkTwitchClips.bind(this), this.twitchClipRefreshRate);
+      })
+      .catch(Shubot.log.error);
+  }
+
+  private getTwitchClips(): Promise<TwitchAPI.Clip[]> {
+    return axios
+      .get('https://api.twitch.tv/kraken/clips/top?channel=Leaflit&period=day&limit=100', {
+        headers: {
+          Accept: 'application/vnd.twitchtv.v5+json',
+          'Client-ID': this.twitchClientId,
+        },
+      })
+      .then(res => {
+        // sort the clips by date
+        const clips: TwitchAPI.Clip[] = res.data.clips || [];
+        clips.sort((first, second) => {
+          const firstDate = new Date(first.created_at).getTime();
+          const secondDate = new Date(second.created_at).getTime();
+          return -(firstDate - secondDate);
+        });
+        return clips;
+      });
+  }
+
+  private checkTwitchClips(): void {
+    this.getTwitchClips()
+      .then(clips => {
+        const clipstopost: TwitchAPI.Clip[] = [];
+        // check for new clips
+        for (let i = 0; i < clips.length; i++) {
+          // since clips is sorted by date ignore older clips
+          if (new Date(clips[i].created_at).getTime() <= this.lastClipDate.getTime()) {
+            break;
+          } else {
+            Shubot.log.info(`Found '${clips[i].title}'`);
+            clipstopost.push(clips[i]);
+          }
+        }
+        clipstopost.forEach(clip => {
+          this.sendDiscordMessage(
+            this.discordClipChannelId,
+            `https://clips.twitch.tv/${clip.slug}`,
+          );
+        });
+        // update the latest clip to the newest one
+        if (clips.length > 0) {
+          this.lastClipDate = new Date(clips[0].created_at);
+        }
+      })
+      .catch(Shubot.log.error);
+  }
+}

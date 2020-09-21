c284ce2111c6cfb52b5998d1c7d94439386aa907
Arimil <renari@arimil.com>
2020-09-21 10:09:16 -0700

    refactor: clean up some code in youtube notification

--- src/notification-handlers/youtube-notification.ts
+++ src/notification-handlers/youtube-notification.ts
@@ -8,6 +8,7 @@ import latestYoutubeVideoDate from '../interfaces/latestYoutubeVideoDate';
 
 export default class youtubeNotification extends notificationHandler {
   private readonly database: Nedb;
+  private readonly databaseType = 'latestYoutubeVideoDate';
   private readonly discordChannelId: string = '725449150573051926';
   private readonly youtubeInstance: youtube;
   private readonly youtubeVideoRefreshRate = 30000;
@@ -20,47 +21,53 @@ export default class youtubeNotification extends notificationHandler {
   }
 
   private checkForNewYoutubeVideos(): void {
-    this.database.findOne<latestYoutubeVideoDate>(
-      { type: 'latestYoutubeVideoDate' },
-      (err, result) => {
-        if (!result) {
-          // we've never got a youtube video before, try to get the latest one
-          this.youtubeInstance.GetLatestVideoDate().then(date => {
-            if (date) {
-              // store this date in the database
-              this.database.update(
-                { type: 'latestYoutubeVideoDate' },
-                { type: 'latestYoutubeVideoDate', date },
-                { upsert: true },
-              );
-            }
-          });
-        } else {
-          // check for newer videos
-          this.youtubeInstance.GetVideosAfterDate(result.date).then(videos => {
-            if (videos && videos.items) {
-              // post the oldest videos first
-              for (let i = videos.items.length - 1; i >= 0; i--) {
-                const video = videos.items[i].id?.videoId;
-                if (video) {
-                  // post new videos to discord
-                  const title = videos.items[i].snippet?.title
-                    ? videos.items[i].snippet?.title
-                    : video;
-                  Shubot.log.info(`Found YouTube video ${title}`);
-                  this.sendDiscordMessage(this.discordChannelId, `https://youtu.be/${video}`);
-                }
+    this.getLastVideoDate().then(date => {
+      if (!date) {
+        // we've never got a youtube video before, try to get the latest one
+        this.youtubeInstance.GetLatestVideoDate().then(date => {
+          if (date) {
+            // store this date in the database
+            this.database.update(
+              { type: this.databaseType },
+              { type: this.databaseType, date },
+              { upsert: true },
+            );
+          }
+        });
+      } else {
+        // check for newer videos
+        this.youtubeInstance.GetVideosAfterDate(date).then(videos => {
+          if (videos && videos.items) {
+            // post the oldest videos first
+            for (let i = videos.items.length - 1; i >= 0; i--) {
+              const video = videos.items[i].id?.videoId;
+              if (video) {
+                // post new videos to discord
+                const title = videos.items[i].snippet?.title
+                  ? videos.items[i].snippet?.title
+                  : video;
+                Shubot.log.info(`Found YouTube video ${title}`);
+                this.sendDiscordMessage(this.discordChannelId, `https://youtu.be/${video}`);
               }
-              // update our latest video date
-              this.database.update(
-                { type: 'latestYoutubeVideoDate', date: result.date },
-                { type: 'latestYoutubeVideoDate', date: videos.items[0].snippet?.publishedAt },
-                { upsert: true },
-              );
             }
-          });
-        }
-      },
-    );
+            // update our latest video date
+            this.database.update(
+              { type: this.databaseType, date: date },
+              { type: this.databaseType, date: videos.items[0].snippet?.publishedAt },
+              { upsert: true },
+            );
+          }
+        });
+      }
+    });
+  }
+
+  private getLastVideoDate(): Promise<string | null> {
+    return new Promise((resolve, reject) => {
+      this.database.findOne<latestYoutubeVideoDate>({ type: this.databaseType }, (err, result) => {
+        reject(err);
+        resolve(result ? result.date : null);
+      });
+    });
   }
 }

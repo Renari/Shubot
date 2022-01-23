bd493424f77b8416943acf6f3c02c92624a973c3
Arie Miller <renari@arimil.com>
2022-01-23 16:44:41 -0500

    fix: update youtube notification to sqlite
    resolves #178

--- src/notification-handlers/youtube-notification.ts
+++ src/notification-handlers/youtube-notification.ts
@@ -1,3 +1,4 @@
+import { Database } from 'better-sqlite3';
 import Discord from 'discord.js';
 import notificationHandler from './notification-handler';
 import Shubot from '..';
@@ -7,72 +8,69 @@ import youtube from '../youtube';
 import latestYoutubeVideoDate from '../interfaces/latestYoutubeVideoDate';
 
 export default class youtubeNotification extends notificationHandler {
-  private readonly database: Nedb;
-  private readonly databaseType = 'latestYoutubeVideoDate';
+  private readonly database: Database;
+  private readonly databaseTableName = 'latestYoutubeVideoDate';
   private readonly discordChannelId: string = '725449150573051926';
   private readonly youtubeInstance: youtube;
   private readonly youtubeVideoRefreshRate = 3600000; // once per hour
 
-  constructor(discordClient: Discord.Client, channelId: string, apiKey: string, database: Nedb) {
+  private lastCheckDate: Date;
+
+  constructor(
+    discordClient: Discord.Client,
+    channelId: string,
+    apiKey: string,
+    database: Database,
+  ) {
     super(discordClient);
     this.database = database;
     this.youtubeInstance = new youtube(apiKey, channelId);
+
+    // make sure our database table exists
+    database
+      .prepare(
+        `CREATE TABLE IF NOT EXISTS ${this.databaseTableName}
+         (
+             date
+             TEXT
+             NOT
+             NULL
+         );`,
+      )
+      .run();
+
+    // get the last time we checked for clips
+    const row = this.database.prepare(`SELECT date FROM ${this.databaseTableName}`).get();
+    this.lastCheckDate = row && row.date ? new Date(row.date) : new Date();
+
     setInterval(this.checkForNewYoutubeVideos.bind(this), this.youtubeVideoRefreshRate);
   }
 
   private checkForNewYoutubeVideos(): void {
-    this.getLastVideoDate()
-      .then((date) => {
-        if (!date) {
-          // we've never got a youtube video before, try to get the latest one
-          this.youtubeInstance.GetLatestVideoDate().then((date) => {
-            if (date) {
-              // store this date in the database
-              this.database.update(
-                { type: this.databaseType },
-                { type: this.databaseType, date },
-                { upsert: true },
-              );
-            }
-          });
-        } else {
-          // check for newer videos
-          this.youtubeInstance.GetVideosAfterDate(date).then((videos) => {
-            if (videos && videos.items) {
-              // post the oldest videos first
-              for (let i = videos.items.length - 1; i >= 0; i--) {
-                const video = videos.items[i].id?.videoId;
-                if (video && videos.items[0].snippet?.publishedAt != date) {
-                  // post new videos to discord
-                  const title = videos.items[i].snippet?.title
-                    ? videos.items[i].snippet?.title
-                    : video;
-                  Shubot.log.info(`Found YouTube video ${title}`);
-                  Shubot.log.info(
-                    `Published At: ${videos.items[0].snippet?.publishedAt}, Previous Date: ${date}`,
-                  );
-                  this.sendDiscordMessage(this.discordChannelId, `https://youtu.be/${video}`);
-                }
-              }
-              // update our latest video date
-              this.database.update(
-                { type: this.databaseType, date: date },
-                { type: this.databaseType, date: videos.items[0].snippet?.publishedAt },
-                { upsert: true },
-              );
-            }
-          });
+    // check for newer videos
+    this.youtubeInstance.GetVideosAfterDate(this.lastCheckDate.toISOString()).then((videos) => {
+      if (videos && videos.items) {
+        // post the oldest videos first
+        for (let i = videos.items.length - 1; i >= 0; i--) {
+          const video = videos.items[i].id?.videoId;
+          // post new videos to discord
+          const title = videos.items[i].snippet?.title ? videos.items[i].snippet?.title : video;
+          Shubot.log.info(`Found YouTube video ${title}`);
+          this.sendDiscordMessage(this.discordChannelId, `https://youtu.be/${video}`);
         }
-      })
-      .catch(Shubot.log.error);
+        // update our latest video date
+        this.upsertDate();
+      }
+    });
   }
 
-  private getLastVideoDate(): Promise<string | null> {
-    return new Promise((resolve, reject) => {
-      this.database.findOne<latestYoutubeVideoDate>({ type: this.databaseType }, (err, result) => {
-        if (err) reject(err);
-        resolve(result ? result.date : null);
-      });
-    });
+  private upsertDate(): void {
+    this.lastCheckDate = new Date();
+    this.database
+      .prepare(`UPDATE ${this.databaseTableName} SET date = ?;`)
+      .run(this.lastCheckDate.toISOString());
+    this.database
+      .prepare(`INSERT INTO ${this.databaseTableName} (date) SELECT ? WHERE (SELECT Changes() = 0)`)
+      .run(this.lastCheckDate.toISOString());
   }
 }
